51aa28e3c9cfd1b6614b13cc3b4a92b807224ab5
Habibur Rohman <107332516+Habiburrohman89@users.noreply.github.com>
2022-07-19 20:19:07 +0700

    Add files via upload

---
+++ habib/habib/views.py
@@ -0,0 +1,12 @@
+from django.http import HttpResponse
+from django.shortcuts import render
+
+def index(request):
+    return render(request,'tahu.html')
+
+
+
+def about(request):
+    return render(request,'about.html')
+
+    
